783c948ae8dcaf51c41fc82cfca1e480cfb82376
FunTom <vogeltanz@utb.cz>
2022-02-09 13:16:37 +0100

    Přidejte soubory projektu.

---
+++ Hrdina a drak - streda 12/Drak.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrdina_a_drak___streda_12
+{
+    public class Drak
+    {
+        public string Jmeno { get; set; }
+        public double Zdravi { get; set; }
+        public double ZdraviMax { get; set; }
+        public double PoskozeniMax { get; set; }
+        public double ZbrojMax { get; set; }
+
+        public Drak(string jmeno, double zdravi, double zdraviMax, double poskozeniMax, double zbrojMax)
+        {
+            Jmeno = jmeno;
+            Zdravi = zdravi;
+            ZdraviMax = zdraviMax;
+            PoskozeniMax = poskozeniMax;
+            ZbrojMax = zbrojMax;
+        }
+
+        public double Utok(Hrdina oponent)
+        {
+            double hodnotaUtoku = 0;
+
+            Random rnd = new Random();
+            hodnotaUtoku = rnd.NextDouble() * PoskozeniMax;
+            hodnotaUtoku -= oponent.Obrana();
+            oponent.SnizZdravi(hodnotaUtoku);
+
+            return hodnotaUtoku;
+        }
+
+        public double Obrana()
+        {
+            double hodnotaObrany = 0;
+
+            //dodelat
+
+            return hodnotaObrany;
+        }
+
+        public void SnizZdravi(double hodnotaSnizeni)
+        {
+            if (hodnotaSnizeni > 0)
+            {
+                Zdravi -= hodnotaSnizeni;
+            }
+        }
+
+        public bool JeZivy()
+        {
+            if (Zdravi > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
